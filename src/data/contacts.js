954bede2b9d0f11263767b6650b25c3beb33551d
Chung Kin Wai <73325564+chungkw@users.noreply.github.com>
2026-03-14 02:11:02 +0800

    Add new project showcase and contacts

---
+++ src/data/contacts.js
@@ -0,0 +1,12 @@
+export const contacts = [
+    {
+        name: "GitHub",
+        icon: "bi-github",
+        link: "https://github.com/chungkw"
+    },
+    {
+        name: "Email",
+        icon: "bi-envelope-fill",
+        link: "mailto:chungkw28@gmail.com"
+    }
+];
