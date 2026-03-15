be98b7f26619cee93cd2a42102dbcf4d2a251341
Chung Kin Wai <73325564+chungkw@users.noreply.github.com>
2026-03-16 00:04:27 +0800

    Update project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
         title: "Discord Bot",
         icon: "bi-discord",
         description:
-            "A project primarily for fun. It is also a really good way to learn and experiment, with visual interactivity. Made with JavaScript.",
+            "A project to learn and experiment with JavaScript and NodeJS, with Discord's interactivity. It features image manipulation and audio/voice playback. Also, perhaps more importantly, for fun and silly.",
         links: [
             {
                 url: "https://github.com/chungkw/discord-bot",
@@ -15,7 +15,7 @@ export const projects = [
         title: "Duty Planner",
         icon: "bi-calendar-check",
         description:
-            "Simple program to draw a planner for duty planning. Planner is drawn in Excel to visualize data. Made with Python.",
+            "Simple program to draw a planner for duty planning. Planner is drawn in Excel to visualize data. Data is read from a directory of the user's choice. Made with Python.",
         links: [
             {
                 url: "https://github.com/chungkw/duty-planner-gui",
@@ -27,7 +27,7 @@ export const projects = [
         title: "Sequelize ORM Tutorial",
         icon: "bi-book",
         description:
-            "A tutorial on using Sequelize, an object-relational mapping tool in JavaScript, for NodeJS-powered backend. Written during my time in Polytechnic, shared with my classmates.",
+            "A tutorial on using Sequelize, an object-relational mapping tool in JavaScript, for NodeJS-powered backend. Written during my time in Polytechnic, shared with my classmates. Includes an example Express backend server to demonstrate concepts.",
         links: [
             {
                 url: "https://github.com/chungkw/sequelize_tutorial",
