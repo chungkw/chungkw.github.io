155d58cc41cca2cf3d2e1cfaa1ac4ec01148100d
Chung Kin Wai <73325564+chungkw@users.noreply.github.com>
2026-03-16 20:36:39 +0800

    Add project technologies

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,7 @@ export const projects = [
         icon: "bi-discord",
         description:
             "A project to learn and experiment with JavaScript and NodeJS, with Discord's interactivity. It features image manipulation and audio/voice playback. Also, perhaps more importantly, for fun and silly.",
+        technologies: ["JavaScript", "NodeJS", "DiscordJS"],
         links: [
             {
                 url: "https://github.com/chungkw/discord-bot",
@@ -15,7 +16,8 @@ export const projects = [
         title: "Duty Planner",
         icon: "bi-calendar-check",
         description:
-            "Simple program to draw a planner for duty planning. Planner is drawn in Excel to visualize data. Data is read from a directory of the user's choice. Made with Python.",
+            "Simple program to draw a planner for duty planning. Planner is drawn in Excel to visualize data. Data is read from a directory of the user's choice.",
+        technologies: ["Python", "Tkinter", "XlxsWriter"],
         links: [
             {
                 url: "https://github.com/chungkw/duty-planner-gui",
@@ -28,6 +30,7 @@ export const projects = [
         icon: "bi-book",
         description:
             "A tutorial on using Sequelize, an object-relational mapping tool in JavaScript, for NodeJS-powered backend. Written during my time in Polytechnic, shared with my classmates. Includes an example Express backend server to demonstrate concepts.",
+        technologies: ["JavaScript", "NodeJS", "Sequelize", "Express"],
         links: [
             {
                 url: "https://github.com/chungkw/sequelize_tutorial",
@@ -39,7 +42,8 @@ export const projects = [
         title: "This Website",
         icon: "bi-globe2",
         description:
-            "A website to present myself and my projects. Uses GitHub Actions to automate deployment. Made with Astro.",
+            "A website to present myself and my projects. Uses GitHub Actions to automate deployment.",
+        technologies: ["Astro", "JavaScript", "HTML", "CSS", "Bootstrap"],
         links: [
             {
                 url: "https://github.com/chungkw/chungkw.github.io",
