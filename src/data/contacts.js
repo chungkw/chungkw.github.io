2d8a1cdd45eed93fa765de2935cbfbd8711ba28c
Chung Kin Wai <73325564+chungkw@users.noreply.github.com>
2026-03-17 23:04:11 +0800

    Add blog page

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,5 +1,10 @@
 export const contacts = [
     {
+        name: "Blog",
+        icon: "bi-braces",
+        link: "/blog"
+    },
+    {
         name: "GitHub",
         icon: "bi-github",
         link: "https://github.com/chungkw"
